eb4439e1a9c19ef3d86d7e91187744c8a746f0e3
techrecursion <ak0266680@gmail.com>
2025-10-14 16:42:24 +0530

    updated test case

--- src/test/java/com/spring/test/test_devops/TestDevopsApplicationTests.java
+++ src/test/java/com/spring/test/test_devops/TestDevopsApplicationTests.java
@@ -2,14 +2,15 @@ package com.spring.test.test_devops;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 class TestDevopsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(true);
+		assertFalse(true);
 	}
 
 }
